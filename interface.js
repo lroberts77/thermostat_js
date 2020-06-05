e74e2427e74c5802f5aa25e5347a212364286049
liam roberts <liamroberts77@googlemail.com>
2020-06-05 15:54:38 +0100

    added api json response in a file to work from

--- interface.js
+++ interface.js
@@ -32,5 +32,25 @@ $(document).ready(function() {
     function updateTemperature(){
         $('#temperature').text(thermostat.temperature);
         $('#temperature').attr('class', thermostat.energyUsage());
-      };
+        };
+
+    function displayWeather(city) {
+        var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+        var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+        var units = '&units=metric';
+        $.get(url + token + units, function(data) {
+            $('#current-temp').text(data.main.temp);
+            $('#current-city').text(data.name);
+            $('#country').text(data.sys.country)
+        })
+    }
+
+    displayWeather('London');
+
+    $('#select-city').submit(function(event) {
+      event.preventDefault();
+      var city = $('#current-cityy').val();
+      displayWeather(city);
+    })
+
 });
